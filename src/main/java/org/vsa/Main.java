16d0619cc0a96fa5eb8f72a01543a9bc9e522e6d
Andrzej Trzaska <atrzaska2@gmail.com>
2014-02-23 11:47:01 +0100

    23-02-2014

--- src/main/java/org/vsa/Main.java
+++ src/main/java/org/vsa/Main.java
@@ -1,8 +1,17 @@
 package org.vsa;
 
+import it.sardegnaricerche.voiceid.utils.wav.WavFile;
+import java.io.File;
+
 public class Main {
+
     public static void main(String[] args) throws Exception {
-        VsaProgram program = new VsaProgram();
-        program.run();
+//        VsaProgram program = new VsaProgram();
+//        program.run();
+
+        // Open the wav file
+        WavFile wavFile = WavFile.openWavFile(new File("wav/yes.wav"));
+        // Display information about the wav file
+        wavFile.display();
     }
 }
